1c2084cfd15a95e53d770a6d2f315d5511f5e3bd
alejandroescaev <alejandroescaev@gmail.com>
2018-09-18 22:40:28 -0500

    Add files via upload

--- analizadorLexicoPhp/phpmlexer.py
+++ analizadorLexicoPhp/phpmlexer.py
@@ -72,29 +72,91 @@ tokens=(
     'WHILE',
 
     #Symbols - simbolos
+    #Aritmeticos
     'SUMA',
     'RESTA',
     'MULTIPLICACION',
     'DIVISION',
+    'ASIGNACION',
+    #Incremento/Decremento
+    'INCREMENTO'
+    'DECREMENTO'
+    #Comparacion
     'MENOR',
+    'MAYOR',
     'MENORIGUAL',
+    'MAYORIGUAL',
+    'DIFERENTE',
     'IGUAL',
-    '',
-    '',
-    '',
-    '',
-    '',
-    '',
-    '',
-    '',
-    '',
-
+    'IGUALTIPODATO',
+    'DIFERENTETIPODATO',
+    'DISTINTO',
+    'SEMICOLON',
+    'COMMA',
+    'LPAREN',
+    'RPAREN',
+    'LBRACKET',
+    'RBRACKET',
+    'LBLOCK',
+    'RBLOCK',
+    'COLON',
+    'AMPERSANT',
+    'HASHTAG',
+    'DOT'
+)
     #Reglas de expresiones regulares para simbolos simples
-    t_SUMA = r'+'
+    t_SUMA = r'\+'
     t_RESTA = r'-'
-    t_MULTIPLICACION = r'*'
+    t_MULTIPLICACION = r'\*'
     t_DIVISION = r'/'
+    t_ASIGNACION = r'='
     t_MENOR = r'<'
-    t_MENORIGUAL = r'<='
-    t_IGUAL = r'='
-    )
+    t_MAYOR = r'>'  
+    t_SEMICOLON = ';'
+    t_COMMA  = r','
+    t_LPAREN = r'\('
+    t_RPAREN  = r'\)'
+    t_LBRACKET = r'\['
+    t_RBRACKET = r'\]'
+    t_LBLOCK   = r'{'
+    t_RBLOCK   = r'}'
+    t_COLON   = r':'
+    t_AMPERSANT = r'\&'
+    t_HASHTAG = r'\#'
+    t_DOT = r'\.'
+
+def t_INCREMENTO(t):
+    r'\+\+'
+    return t
+
+def t_DECREMENTO(t):
+    r'--'
+    return t
+
+def t_MENORIGUAL(t):
+    r'<='
+    return t
+
+def t_MAYORIGUAL(t):
+    r'>='
+    return t
+
+def t_DIFERENTE(t):
+    r'<>'
+    return t
+
+def t_IGUAL(t):
+    r'=='
+    return t
+    
+def t_IGUALTIPODATO(t):
+    r'==='
+    return t
+
+def t_DIFERENTETIPODATO(t):
+    r'!=='
+    return t
+
+def t_DISTINTO(t):
+    r'!='
+    return t
